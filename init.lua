96d9c1e22be7471433ef9fa0c79ae5dcaa21f942
Robbie Ferguson <contact@category5.tv>
2017-08-02 20:13:48 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,8 @@
--- will go here
+minetest.register_node(":technic:chernobylite_block", {
+        description = S("Chernobylite Block (Degriefed)"),
+        tiles = {"technic_chernobylite_block.png"},
+        is_ground_content = true,
+        groups = {cracky=1, level=2},
+        sounds = default.node_sound_stone_defaults(),
+        light_source = 2,
+})
