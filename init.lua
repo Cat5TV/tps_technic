7b4a0eeb92a5dc781b88b70e56f4ab644de2e640
Robbie Ferguson <contact@category5.tv>
2017-08-10 15:50:59 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -7,6 +7,24 @@ minetest.register_node(":technic:chernobylite_block", {
         light_source = 2,
 })
 
+minetest.register_node( ":technic:mineral_uranium", {
+        description = S("Uranium Ore"),
+        tiles = { "default_stone.png^technic_mineral_uranium.png" },
+        is_ground_content = true,
+        groups = {cracky=3, radioactive=0},
+        sounds = default.node_sound_stone_defaults(),
+        drop = "technic:uranium_lump",
+})
+
+minetest.register_node(":technic:uranium_block", {
+        description = S("Uranium Block"),
+        tiles = { "technic_uranium_block.png" },
+        is_ground_content = true,
+        groups = {uranium_block=1, cracky=1, level=2, radioactive=0},
+        sounds = default.node_sound_stone_defaults()
+})
+
+
 minetest.override_item("technic:corium_source", {
    after_place_node = function(pos, placer, itemstack, pointed_thing)
          minetest.remove_node(pos)
@@ -14,3 +32,6 @@ minetest.override_item("technic:corium_source", {
 })
 
 minetest.unregister_item("technic:bucket_corium")
+
+-- Temporary, until can be added to priv
+minetest.unregister_item("technic:lava_can")
