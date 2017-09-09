a55b27df272732e59701c2225f0f9310c792b42a
Robbie Ferguson <contact@category5.tv>
2017-09-09 10:43:51 -0400

    try uranium as alias

--- init.lua
+++ init.lua
@@ -8,8 +8,10 @@ minetest.register_node(":technic:chernobylite_block", {
 })
 
 -- Remove Uranium altogether
-minetest.unregister_item("technic:mineral_uranium")
-minetest.unregister_item("technic:uranium_block")
+minetest.register_alias("technic:mineral_uranium","air")
+minetest.register_alias("technic:uranium_block","air")
+--minetest.unregister_item("technic:mineral_uranium")
+--minetest.unregister_item("technic:uranium_block")
 
 
 minetest.override_item("technic:corium_source", {
