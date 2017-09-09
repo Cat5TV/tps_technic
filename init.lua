6270bc677af0be0cec8fd4fd77c3a36cfc8ec939
Robbie Ferguson <contact@category5.tv>
2017-09-09 10:46:35 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -8,11 +8,10 @@ minetest.register_node(":technic:chernobylite_block", {
 })
 
 -- Remove Uranium altogether
-minetest.register_alias("technic:mineral_uranium","air")
-minetest.register_alias("technic:uranium_block","air")
 minetest.unregister_item("technic:mineral_uranium")
 minetest.unregister_item("technic:uranium_block")
-
+minetest.register_alias("technic:mineral_uranium","air")
+minetest.register_alias("technic:uranium_block","air")
 
 minetest.override_item("technic:corium_source", {
    after_place_node = function(pos, placer, itemstack, pointed_thing)
